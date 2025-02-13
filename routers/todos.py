5450c23ef29670fcbf028cfa310ebcdcc3161596
Deniz Akar <98228520+denakIST@users.noreply.github.com>
2025-02-13 12:16:48 -0500

    fix commit issue

--- routers/todos.py
+++ routers/todos.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends, HTTPException, status
 import schemas
 import crud
-from database import SessionLocal, engine
+from database import SessionLocal
 
 router = APIRouter(
     prefix="/todos"
